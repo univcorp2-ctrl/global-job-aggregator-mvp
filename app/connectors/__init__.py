77befb0ff020fee4cc0f62f36c816ea0a56f0d7e
univcorp2-ctrl <univcorp2@gmail.com>
2026-05-22 14:49:05 +0900

    Build safety-first global job aggregation MVP

---
+++ app/connectors/__init__.py
@@ -0,0 +1,21 @@
+from app.connectors.arbeitnow import ArbeitnowConnector
+from app.connectors.ashby import AshbyConnector
+from app.connectors.freelancer import FreelancerConnector
+from app.connectors.greenhouse import GreenhouseConnector
+from app.connectors.himalayas import HimalayasConnector
+from app.connectors.jobicy import JobicyConnector
+from app.connectors.lever import LeverConnector
+from app.connectors.remotive import RemotiveConnector
+from app.connectors.serpapi import SerpApiConnector
+
+__all__ = [
+    "ArbeitnowConnector",
+    "AshbyConnector",
+    "FreelancerConnector",
+    "GreenhouseConnector",
+    "HimalayasConnector",
+    "JobicyConnector",
+    "LeverConnector",
+    "RemotiveConnector",
+    "SerpApiConnector",
+]
